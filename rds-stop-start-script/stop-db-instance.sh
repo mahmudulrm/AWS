13faab9c8b8d71fe35ca4b8b7d4f0a1c9ea31a20
mahmudulrm <mahmudulrm@gmail.com>
2020-11-27 18:44:01 +0600

    RDS stop and start

---
+++ rds-stop-start-script/stop-db-instance.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+aws configure set aws_access_key_id <key>
+aws configure set aws_secret_access_key <key>
+aws configure set default.region us-west-2
+
+aws rds stop-db-instance --db-instance-identifier db_instance_name --region us-west-2
